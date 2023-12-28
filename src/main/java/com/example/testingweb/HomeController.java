5ec285da3e5c19af39c61d222f489ce7f8237900
mfiueroah <73349149+mfiueroah@users.noreply.github.com>
2023-12-28 14:06:20 -0300

    Update HomeController.java

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
-		string password = "1234";
+		String password = "1234";
 		return "Mensaje Modificado, Manuel Figueroa";
 	}
 
